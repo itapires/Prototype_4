75dfb229bf2b73ac26084ead05a7d82119579ab6
itapires <itapritsch@gmail.com>
2024-05-16 19:17:08 +0100

    5.Add forward force to the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Sphere : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float forwardInout = Input.GetAxis("Vertical");
+        playerRb.AddForce(Vector3.forward * speed * forwardInout);
     }
 }
