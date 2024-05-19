0118ca0ec721148d6dd2971e656f39dd52085013
itapires <itapritsch@gmail.com>
2024-05-19 20:34:32 +0100

    3.Create a lookDirection variable

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sphere : MonoBehaviour
+public class Player : MonoBehaviour
 {
     private Rigidbody playerRb;
     public float speed = 5.0f;
